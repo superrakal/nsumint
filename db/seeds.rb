69f2686dace54da95013dd2e58bfe29c97c4c3ff
Egor <topolnyak012@gmail.com>
2015-12-06 19:37:15 +0600

    #fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Faculty.find_or_create_by(name: 'ГГФ')
+Faculty.find_or_create_by(name: 'ГФ')
+Faculty.find_or_create_by(name: 'МедФ')
+Faculty.find_or_create_by(name: 'ММФ')
+Faculty.find_or_create_by(name: 'ФЖ')
+Faculty.find_or_create_by(name: 'ФЕН')
+Faculty.find_or_create_by(name: 'ФИТ')
+Faculty.find_or_create_by(name: 'ФИЯ')
+Faculty.find_or_create_by(name: 'ФП')
+Faculty.find_or_create_by(name: 'ФФ')
+Faculty.find_or_create_by(name: 'ФилФ')
+Faculty.find_or_create_by(name: 'ЭФ')
+Faculty.find_or_create_by(name: 'ЮФ')
+Faculty.find_or_create_by(name: 'Другой ВУЗ')
+
